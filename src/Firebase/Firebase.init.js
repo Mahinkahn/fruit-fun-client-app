14eb9efb46bc7a17c038d7a38c9aadbf1af18b9b
Mahinkahn <mahinkahn37@gmail.com>
2022-12-15 21:28:42 +0600

    env

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -5,13 +5,22 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+
+  apiKey: "AIzaSyAPaa5fLG8OhB3hOE0p_ce-mes6EEIw6lM",
+
+  authDomain: "fuit-fun.firebaseapp.com",
+
+  projectId: "fuit-fun",
+
+  storageBucket: "fuit-fun.appspot.com",
+
+  messagingSenderId: "333659487704",
+
+  appId: "1:333659487704:web:70ff9441c6900e794c46ad"
+
 };
+
+console.log(process.env.REACT_APP_apiKey)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
